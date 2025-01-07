85dddd68c511591f61a0969185078f85635793cb
Calana2 <kalcast00@proton.me>
2025-01-07 14:51:29 -0500

    Added miniscan

--- backup.sh
+++ backup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # It creates a BACKUP-[DATE].tar.gz file
 
-datetime=$(date +%d-%m-%y\(%H:%M:%S\))
+datetime=$(date +%d-%m-%yT%H-%M-%S)
 name="BACKUP-${datetime}"
 route="/home/$USER/${name}.tar.gz"                        # Change with the route that you want
 
 # Create backup, change with your files/dirs routes
 backupfiles=(
+"/home/$USER/Codigo"
 "/home/$USER/Documentos"
 "/home/$USER/Laboratorio"
 "/home/$USER/.vimrc"
 "/home/$USER/.bashrc"
 "/home/$USER/.config/nvim"
+"/home/$USER/bookmarks.html"
 )
 tar czfv "${route}" "${backupfiles[@]}"
 if [ $? -ne 0 ];then
---
+++ miniscan.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Mini malware scanner
+for arg in $@;do
+ if [ -e $arg ];then
+   if [ -d $arg ];then
+     echo "$arg is a directory"
+   elif [ -f $arg ];then
+     echo -e "\e[1;36m___${arg}___\e[0m"
+     # FileType
+     echo -e "\e[1;32mFileType\e[0m"
+     echo " $(file $arg)"
+     # Permissions
+     echo -e "\e[1;32mPermissions\e[0m"
+     echo " $(ls -lh $arg)"
+     # Checksums
+     echo -e "\e[1;32mChecksums\e[0m"
+     echo " sha256: $(sha256sum $arg | cut -d " " -f 1)"
+     echo " sha1: $(sha1sum $arg | cut -d " " -f 1)"
+     echo " md5: $(md5sum $arg | cut -d " " -f 1)"
+     # Suspicious strings
+     _strings_=$(strings $arg)
+     domains=$(grep -ioE "(http[s]*://(www\.)?[a-zA-Z0-9@:%._\+~#=]+{1,256}\.[a-zA-Z0-9()]{1,6}([-a-zA-Z0-9()@:%_\+.~#?&//=]*))" <<< "$_strings_")
+     ipv4_urls=$(grep -ioE "(http[s]*://([0-9]{1,3}\.){3}[0-9]{1,3})" <<< "$_strings_")
+     ipv4=$(grep -ioE "([0-9]{1,3}\.){3}[0-9]{1,3}" <<< "$_strings_")
+     sus_strings=$(echo -e "$domains\n$ipv4_urls\n$ipv4")
+     if [[ ! -z $sus_strings ]];then
+        echo -e "\e[1;32mSuspicious strings\e[0m"
+        echo $sus_strings | tr " " "\n" | while IFS= read -r line; do echo " $line"; done
+     fi
+    echo 
+   fi
+ else 
+   echo "$arg don't exists"
+ fi
+done
